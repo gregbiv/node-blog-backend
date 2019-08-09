e82540bcbfb35649c9606e88d03cd3eac51eb6fe
Gregory Kornienko <gregbiv@gmail.com>
2019-08-09 18:07:27 +0200

    Add remove posts endpoint

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,7 @@
 const Routes = {
     'GET /posts': 'PostsController.getAll',
     'POST /posts': 'PostsController.create',
+    'DELETE /posts/:id': 'PostsController.remove',
 };
 
 module.exports = Routes;
